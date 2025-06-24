f7c57b125ed1978d92bcb6daf4e4c166ae4f622f
Gagan Sidhu <gagansidhu@flash.co>
2025-06-24 02:47:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const inter = Inter({
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '600', '700'],
+  weight: ['400', '600'],
   variable: '--font-poppins',
   display: 'swap',
 });
