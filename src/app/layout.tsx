6782c31f569f8790441253161f0aacba85ee9313
Gagan Sidhu <gagansidhu@flash.co>
2025-06-28 09:48:42 +0000

    Ill done it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Grock | Instant Gift Cards & Memberships',
     description: 'Instantly buy and send digital gift cards and membership vouchers for top brands.',
-    url: 'https://grock.fun',
+    url: 'https://www.your-domain.com',
     siteName: 'Grock',
     images: [
       {
